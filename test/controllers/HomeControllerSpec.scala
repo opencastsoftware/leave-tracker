c33f4c2a627f0f54701ecedcd75c8319f8b5275d
Steve Hobbs <elkdanger@gmail.com>
2018-01-18 13:32:27 +0000

    Added scalatest + basic example test for HomeController

---
+++ test/controllers/HomeControllerSpec.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import org.scalatest.{MustMatchers, WordSpec}
+import play.api.test.Helpers._
+import play.api.test.{FakeRequest, Helpers}
+
+class HomeControllerSpec extends WordSpec with MustMatchers {
+  "The index action" must {
+    "return an OK 200 response" in {
+      val controller = new HomeController(Helpers.stubControllerComponents())
+      status(controller.index()(FakeRequest())) mustBe OK
+    }
+  }
+
+}
